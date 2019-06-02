edad4b45edfbd5a39d38a3ae9e6da000fbc01fbd
Lucas Emanuel Resck Domingues <lucasemanuelresck@gmail.com>
2019-06-01 21:24:33 -0300

    Changed data structure to a trie of the words

--- Indexer.cpp
+++ Indexer.cpp
@@ -1,109 +1,75 @@
 #include <iostream>
 #include <fstream>
 #include <time.h>
+#include <vector>
 
 using namespace std;
 
 struct Node {
-	string alphabet[128];
+	vector<int> documents;
+	//string alphabet[128];
 	Node* pChild[128];
 	Node() {
 		for (int i = 0; i < 128; i++) {
-			alphabet[i] = "";
+			//alphabet[i] = "";
 			pChild[i] = nullptr;
 		}
 	}
 };
 
-class SuffixTree {
+class Trie {
 public:
 	
 	Node* pRoot = new Node;
 	
-	void add_string(string word, Node* pNode) {
-		int word_ant_length = word.length() - 1;
-		string word_ant = word.substr(0, word_ant_length);
-		char new_char = word[word.length() - 1];
-		
-		int i = (int)word[0];
-		
-		string edge = pNode->alphabet[i];
-		int edge_length = edge.length();
-		
-		if (word_ant_length == edge_length) {
-			if (pNode->pChild[i] == nullptr) {
-				//Rule 1
-				pNode->alphabet[i] = word;
-			}
-			else {
-				string new_char_str;
-				new_char_str = new_char;
-				add_string(new_char_str, pNode->pChild[i]);
-			}
-		}
-		else if (word_ant_length > edge_length)
-			add_string(word.substr(edge_length, word_ant_length - edge_length), pNode->pChild[i]);
-		else {
-			char old_char = edge[word_ant_length];
-			if (old_char != new_char) {
-				//Rule 2
-				Node* pAux = new Node;
-				pAux->alphabet[(int)new_char] = new_char;
-				pAux->pChild[(int)old_char] = pNode->pChild[i];
-				pNode->pChild[i] = pAux;
-				pAux->alphabet[(int)old_char] = edge.substr(word_ant_length, edge_length - word_ant_length);
-				pNode->alphabet[i] = word_ant;
-			}
-			else
-				return; //Rule 3	
+	Trie() {
+	}
+	
+	void add_page(string page, int doc) {
+		int init, end;
+		int letter = 0;
+		while (letter < page.length()) {
+			while (page[letter] == ' ' && letter < page.length()) letter++;
+			init = letter;
+			while (page[letter] != ' ' && letter < page.length()) letter++;
+			end = letter - 1;
+			if (init <= end) add_word(page.substr(init, end - init + 1), pRoot, doc);
 		}
 	}
 	
-	void add_page(string page) {
-		int m = page.length();
-		time_t init = time(NULL);
-		time_t now;
-		cout << m << endl;
-		for (int phase = 1; phase <= m; phase++) {
-		//cout << "Phase" + to_string(phase) << endl;
-			for (int extension = 1; extension <= phase; extension++) {
-				//cout << "Extension " + to_string(extension) << endl;
-				add_string(page.substr(extension - 1, phase - extension + 1), pRoot);
-				//cout << endl;
-				now = time(NULL);
-				if (now - init > 1) {
-					init = now;
-					cout << "Phase " << to_string(phase) << "; extension " << to_string(extension)<< endl;
-				}
-			}
+	void add_word(string word, Node* pInit, int doc) {
+		if (word == "") {
+			if (pInit->documents.size() == 0)
+				pInit->documents.push_back(doc);
+			else if (pInit->documents[pInit->documents.size() - 1] != doc)
+					pInit->documents.push_back(doc);
+			return;
 		}
+		if (pInit->pChild[(int)word[0]] == nullptr)
+			pInit->pChild[(int)word[0]] = new Node;
+		add_word(word.substr(1, word.length() - 1), pInit->pChild[(int)word[0]], doc);
 	}
-	
-	SuffixTree() {
-		
-	}
-	
 };
 
 int main() {
-time_t init = time(NULL);
+	time_t init = time(NULL);
 	time_t now;
-	SuffixTree st;
-	for (int i = 1; i < 5; i++) {
-		if (i % 1 == 0) {
+	Trie trie;
+	for (int doc = 0; doc < 1359870; doc++) {
+		if (doc % 100 == 0) {
 			now = time(NULL);
-			cout << i << endl;
+			cout << doc << endl;
 			cout << now - init;
 			cout << " s = ";
 			cout << (now - init) / 60;
 			cout << " min" << endl << endl;
 		}
 		string page;
-		ifstream file ("Cleaned_Texts/" + to_string(i) + ".txt");
+		ifstream file ("Cleaned_Texts/" + to_string(doc) + ".txt");
 		if (file.is_open()) {
 			if (file.good()) {
 				getline(file, page);
-				st.add_page(page);
+				trie.add_page(page, doc);
 			}
 		}
 	}
